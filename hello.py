88d9eb6c3a677956bce70d5451ca60c4aad93b11
walshbr <bmw9t@virginia.edu>
2017-06-01 13:31:10 -0400

    slides

---
+++ hello.py
@@ -0,0 +1,10 @@
+from sys import argv
+
+if len(argv) == 1:
+    print('Hello, World!')
+else:
+    print('Hello, ' + argv[1])
+
+# print(argv)
+# script, user_name = argv
+# print("Hello" + user_name)
